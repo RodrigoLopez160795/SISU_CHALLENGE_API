47e8d4347c51d9212da02e18bf5d14cfeccaa102
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-12-22 23:33:04 -0600

    Add validations to models

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,4 @@
 class Country < ApplicationRecord
+  has_many :states
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   belongs_to :country
+  validates :name, length: { maximum: 50 }
+  validate :validate_age
+  def validate_age
+    return if age >= 18 && age <= 99
+
+    errors.add :user, :invalid, message: 'El usuario debe tener entre 18 y 99 años'
+  end
 end
